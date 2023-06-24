a95f3c22d3ecc6f60095687fd2d4affe1c9c3109
sakibsarker <sakib15-3824@diu.edu.bd>
2023-06-24 18:53:33 +0600

    nothing

--- restwork/views.py
+++ restwork/views.py
@@ -56,5 +56,23 @@ class AdvocateDetails(APIView):
         advocate.delete()
         return Response('user was deleted')
 
-    
-        
+
+@api_view(['GET','PUT','DELETE'])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def advocate_details(request,username):
+    try:
+        advocate=Advocate.objects.get(username=username)
+        if request.method=="GET":
+            serializer=AdvocateSerializer(advocate,many=False)
+            return Response(serializer.data)
+        if request.method=='PUT':
+            advocate.username=request.data['username']
+            advocate.bio=request.data['bio']
+            advocate.save()
+            serializer=AdvocateSerializer(advocate,many=False)
+            return Response(serializer.data)
+        if request.method=='DELETE':
+            advocate.delete()
+            return Response('delete done')
+    except ObjectDoesNotExist:
+        return Response({'error': 'Advocate username not found.'}, status=404)
