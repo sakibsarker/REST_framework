5629df8f226042bde43f5110b14d4f73f1f710e3
sakibsarker <sakib15-3824@diu.edu.bd>
2023-07-03 13:17:52 +0600

    cors

--- restwork/urls.py
+++ restwork/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,TokenRefreshView
+)
 urlpatterns = [
     path('',views.enpoint),
     path('advocates/',views.advocate_list,name='advocates'),
     path('advocates/<str:username>/',views.AdvocateDetails.as_view()),
     # path('advocates/<str:username>/',views.advocate_details)
     path('companis/',views.Companis_List,name='companis'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
--- restwork/views.py
+++ restwork/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import JsonResponse
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
 from .models import Advocate,Company
 from .serializers import AdvocateSerializer,CompanySerializer
 from django.core.exceptions import ObjectDoesNotExist
@@ -12,7 +12,7 @@ from django.http import Http404
 
 # Create your views here.
 @api_view(['GET'])
-@permission_classes([IsAuthenticatedOrReadOnly])
+@permission_classes([IsAuthenticated])
 def enpoint(request):
     data=['advocates','advocates/:username']
     return Response(data)
